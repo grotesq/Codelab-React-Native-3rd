23559c4f2dc5240b12a53f0f0269229ed5e165b4
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2019-08-17 11:57:49 +0900

    TodoList 스타일링 추가

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import _ from 'underscore';
+import styled from 'styled-components/native';
+
+const Row = styled.View`
+  flex-direction: row;
+`
+const Input = styled.TextInput`
+  width: 300;
+  border-color: #000;
+  border-bottom-width: 1;
+`
+const TodoItem = styled.View`
+  flex-direction: row;
+  width: 350;
+  justify-content: space-between;
+`
+const CustomButton = styled.TouchableOpacity`
+  
+`
+/*
+<View style={{ flexDirection: 'row', width: 350, ... }}/>
+<TodoItem/>
+*/
 
 export default function App() {
-  const [ content, setContent ] = useState( 'Text Here' );
+  const [ content, setContent ] = useState( '' );
   const [ list, setList ] = useState( [] );
   const addItem = () => {
     const item = {
@@ -17,19 +39,28 @@ export default function App() {
     // https://underscorejs.org/#reject
     setList( _.reject( list, item => item.id === id ) );
   }
+  // const inputStyle = {
+  //   width: 300,
+  //   borderColor: "#000",
+  //   borderBottomWidth: 1,
+  // }
   return (
-    <View style={styles.container}>
-      <TextInput value={ content }
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={ [ styles.row, { marginBottom: 12 } ] }>
+          <Input value={ content }
                  onChangeText={ text => setContent( text ) }
-                 style={{ width: 300 }}/>
-      <Button title="추가" onPress={ () => addItem() }/>
-      { list.map( item => (
-        <View key={ item.id } style={{flexDirection: 'row'}}>
-          <Text>{ item.content }</Text>
-          <Button color={'#f00'} title="삭제" onPress={ () => remove( item.id ) }/>
+                 />
+          <Button title="추가" onPress={ () => addItem() }/>
         </View>
-      ) ) }
-    </View>
+        { list.map( item => (
+          <TodoItem key={ item.id }>
+            <Text>{ item.content }</Text>
+            <Button color={'#f00'} title="삭제" onPress={ () => remove( item.id ) }/>
+          </TodoItem>
+        ) ) }
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -38,6 +69,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
+  row: {
+    flexDirection: 'row'
+  },
+  todoItem: {
+    width: 350,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputStyle: {
+    width: 300,
+    borderColor: "#000",
+    borderBottomWidth: 1,
+  }
 });
